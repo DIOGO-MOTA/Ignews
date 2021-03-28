1785c260e6a579ca639d81c0d5c2d7d030a2ca0f
DIOGO-MOTA <diogomota21@gmail.com>
2021-03-28 12:33:08 -0300

    Nextauth

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from "next/head";
+
 import { SubscribeButton } from "../components/SubscribeButton";
 import { stripe } from '../services/stripe';
 
@@ -37,7 +38,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1IZkzGIgz2xgvH7X9N88Rwc5', {
     expand: ['product']
   })
@@ -54,6 +55,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product,
-    }
+    },
+    revalidate: 60 * 60 * 24, //24hours
   }
 }
